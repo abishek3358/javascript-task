d77793448354af3bb3e7b3db00302af040790e70
Abishek A <abishek582000@gmail.com>
2022-11-03 14:09:59 +0530

    space removed

--- Characters calc.js
+++ Characters calc.js
@@ -1,15 +1,15 @@
-function calculate() {
-    var textarea = document.getElementById('textarea')
+function calculate(){
+    var textarea = document.getElementById('textarea');
     if (textarea.value == '') {
-        alert('Textarea was empty!')
+        alert('Textarea was empty!');
     }
     else {
-        var wrk = document.getElementById('work')
-        wrk.className = 'col-lg-6'
-        var textarea = document.getElementById('textarea')
-        var output = document.getElementById('anwers')
-        arrayValue = textarea.value.trim(' ').split(' ').join('')
-        inputArray = arrayValue.split('')
+        var wrk = document.getElementById('work');
+        wrk.className = 'col-lg-6';
+        var textarea = document.getElementById('textarea');
+        var output = document.getElementById('anwers');
+        arrayValue = textarea.value.trim(' ').split(' ').join('');
+        inputArray = arrayValue.split('');
         for (let i = 0; i < inputArray.length; i++) {
             let count = 0;
             for (let j = 0; j < inputArray.length; j++) {
@@ -23,16 +23,13 @@ function calculate() {
             if (count > 0) {
                 var output = document.getElementById('anwers')
                 if (output.innerHTML == '') {
-                    output.innerHTML = '<h2><p>This input contains <b>' + inputArray.length + '</b> characters.</p></h2>' + '<p>In this input includes ' + count + ' ' + '<b>' + inputArray[i].toUpperCase() + '</b>' + ' characters.</p>'
+                    output.innerHTML = '<h2><p>This input contains <b>' + inputArray.length + '</b> characters.</p></h2>' + '<p>In this input includes ' + count + ' ' + '<b>' + inputArray[i].toUpperCase() + '</b>' + ' characters.</p>';
                 }
                 else {
-                    output.innerHTML += '<p>In this input includes ' + count + ' ' + '<b>' + inputArray[i].toUpperCase() + '</b>' + ' characters.</p>'
+                    output.innerHTML += '<p>In this input includes ' + count + ' ' + '<b>' + inputArray[i].toUpperCase() + '</b>' + ' characters.</p>';
                 }
             }
         }
-
-
     }
-
 }
 
--- Input calc.js
+++ Input calc.js
@@ -1,55 +1,51 @@
-
 function calculate() {
-    let firstInputValue = document.getElementById("input1")
-    secondInputValue = document.getElementById("input2")
+    let firstInputValue = document.getElementById("input1");
+    secondInputValue = document.getElementById("input2");
     if (firstInputValue.value ,secondInputValue.value  == '') {
-        alert('Empty inputs not allowed')
+        alert('Empty inputs not allowed');
     }
     else {
         //Addition
-        let valueAdd = Number(firstInputValue.value) + Number(secondInputValue.value)
-        var addValue = document.getElementById("addvalue")
-        addValue.innerHTML = "Addition of this inputs is <b>A + B =</b>" + '<b>' + valueAdd + '</b>'
+        let valueAdd = Number(firstInputValue.value) + Number(secondInputValue.value);
+        var addValue = document.getElementById("addvalue");
+        addValue.innerHTML = "Addition of this inputs is <b>A + B =</b>" + '<b>' + valueAdd + '</b>';
         //Subtraction
-        let valueSub = Number(firstInputValue.value) - Number(secondInputValue.value)
-        var subValue = document.getElementById("subvalue")
-        subValue.innerHTML = "Subtraction of this inputs is <b>A - B =</b>" + '<b>' + valueSub + '</b>'
+        let valueSub = Number(firstInputValue.value) - Number(secondInputValue.value);
+        var subValue = document.getElementById("subvalue");
+        subValue.innerHTML = "Subtraction of this inputs is <b>A - B =</b>" + '<b>' + valueSub + '</b>';
         //Multiply
-        let valueMultiply = Number(firstInputValue.value) * Number(secondInputValue.value)
-        var multiplyValue = document.getElementById("multiply")
-        multiplyValue.innerHTML = "Multiplication of this inputs is <b>A * B =</b>" + '<b>' + valueMultiply + '</b>'
+        let valueMultiply = Number(firstInputValue.value) * Number(secondInputValue.value);
+        var multiplyValue = document.getElementById("multiply");
+        multiplyValue.innerHTML = "Multiplication of this inputs is <b>A * B =</b>" + '<b>' + valueMultiply + '</b>';
         //Division
-        let valueDivide = Number(firstInputValue.value) / Number(secondInputValue.value)
-        var divisionValue = document.getElementById("division")
-        divisionValue.innerHTML = "Division of this inputs is <b>A / B =</b>" + '<b>' + valueDivide + '</b>'
+        let valueDivide = Number(firstInputValue.value) / Number(secondInputValue.value);
+        var divisionValue = document.getElementById("division");;
+        divisionValue.innerHTML = "Division of this inputs is <b>A / B =</b>" + '<b>' + valueDivide + '</b>';
         //Squared the sum value
-        let valueSquared = valueAdd * valueAdd
-        var squaredValue = document.getElementById("squared")
-        squaredValue.innerHTML = "Squared the sum value of <b>A </b> and <b>B =</b>" + '<b>' + valueSquared + '</b>'
+        let valueSquared = valueAdd * valueAdd;
+        var squaredValue = document.getElementById("squared");
+        squaredValue.innerHTML = "Squared the sum value of <b>A </b> and <b>B =</b>" + '<b>' + valueSquared + '</b>';
         //Square root the sum value
-        let valueSquareoot = Math.sqrt(valueAdd)
-        var squareRootValue = document.getElementById('squareRoot')
-        squareRootValue.innerHTML = "Square root the sum value of <b>A </b> and <b>B =</b>" + '<b>' + valueSquareoot + '</b>'
+        let valueSquareoot = Math.sqrt(valueAdd);
+        var squareRootValue = document.getElementById('squareRoot');
+        squareRootValue.innerHTML = "Square root the sum value of <b>A </b> and <b>B =</b>" + '<b>' + valueSquareoot + '</b>';
         //Logarithm of the sum value
-        let valueLog = Math.log(valueAdd)
-        var logarithmValue = document.getElementById('logvalue')
-        logarithmValue.innerHTML = "Log<b>(A </b> + <b>B) =</b>" + '<b>' + valueLog + '</b>'
+        let valueLog = Math.log(valueAdd);
+        var logarithmValue = document.getElementById('logvalue');
+        logarithmValue.innerHTML = "Log<b>(A </b> + <b>B) =</b>" + '<b>' + valueLog + '</b>';
         //Sin value
-        let valueSin = Math.sin(valueAdd)
-        var SinValue = document.getElementById('sinValue')
-        SinValue.innerHTML = "sin <b>(A </b> + <b>B) =</b>" + '<b>' + valueSin + '</b>'
+        let valueSin = Math.sin(valueAdd);
+        var SinValue = document.getElementById('sinValue');
+        SinValue.innerHTML = "sin <b>(A </b> + <b>B) =</b>" + '<b>' + valueSin + '</b>';
         //cos value
-        let valueCos = Math.cos(valueAdd)
-        var cosValue = document.getElementById('cosValue')
-        cosValue.innerHTML = "cos <b>(A </b> + <b>B) =</b>" + '<b>' + valueCos + '</b>'
+        let valueCos = Math.cos(valueAdd);
+        var cosValue = document.getElementById('cosValue');
+        cosValue.innerHTML = "cos <b>(A </b> + <b>B) =</b>" + '<b>' + valueCos + '</b>';
         //tan value
         let valueTan = Math.tan(valueAdd)
-        var tanValue = document.getElementById('tanValue')
-        tanValue.innerHTML = "tan <b>(A </b> + <b>B) =</b>" + '<b>' + valueTan + '</b>'
+        var tanValue = document.getElementById('tanValue');
+        tanValue.innerHTML = "tan <b>(A </b> + <b>B) =</b>" + '<b>' + valueTan + '</b>';
     }
-
-
-
 }
 
 
